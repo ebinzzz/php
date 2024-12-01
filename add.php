7d82a23aa51c75ad2b8966c61052cfa6ef718d38
ebinzzz <124451946+ebinzzz@users.noreply.github.com>
2024-12-01 13:29:24 +0530

    Update add.php

--- add.php
+++ add.php
@@ -3,3 +3,5 @@ $a=(int)readline("enter the first number");
 $b=(int)readline("enter the second number");
 $c=$a+$b;
 echo "The sum of $a and $b is $c";
+
+//change the operator for your liking to perform specefic operation
